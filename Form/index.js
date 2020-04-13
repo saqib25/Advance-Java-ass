ac50e2da2561ebe28fd44fc0a478cc24dc212439
saqib <saqialisheikh2014@gmail.com>
2020-04-14 00:24:46 +0500

    added files

---
+++ Form/index.js
@@ -0,0 +1,102 @@
+$(document).ready(function(){
+
+    var current_fs, next_fs, previous_fs; //fieldsets
+    var opacity;
+    var current = 1;
+    var steps = $("fieldset").length;
+    
+    setProgressBar(current);
+    
+    $(".next").click(function(){
+    
+    current_fs = $(this).parent();
+    next_fs = $(this).parent().next();
+    
+    //Add Class Active
+    $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+    
+    //show the next fieldset
+    next_fs.show();
+    //hide the current fieldset with style
+    current_fs.animate({opacity: 0}, {
+    step: function(now) {
+    // for making fielset appear animation
+    opacity = 1 - now;
+    
+    current_fs.css({
+    'display': 'none',
+    'position': 'relative'
+    });
+    next_fs.css({'opacity': opacity});
+    },
+    duration: 500
+    });
+    setProgressBar(++current);
+    });
+    
+    $(".previous").click(function(){
+    
+    current_fs = $(this).parent();
+    previous_fs = $(this).parent().prev();
+    
+    //Remove class active
+    $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+    
+    //show the previous fieldset
+    previous_fs.show();
+    
+    //hide the current fieldset with style
+    current_fs.animate({opacity: 0}, {
+    step: function(now) {
+    // for making fielset appear animation
+    opacity = 1 - now;
+    
+    current_fs.css({
+    'display': 'none',
+    'position': 'relative'
+    });
+    previous_fs.css({'opacity': opacity});
+    },
+    duration: 500
+    });
+    setProgressBar(--current);
+    });
+    
+    function setProgressBar(curStep){
+    var percent = parseFloat(100 / steps) * curStep;
+    percent = percent.toFixed();
+    $(".progress-bar")
+    .css("width",percent+"%")
+    }
+    
+    $(".submit").click(function(){
+    return false;
+    })
+    
+    });
+
+var submit= document.querySelector('#submit1');
+  
+    function step1(){
+        var step={
+          Email: document.getElementById('email').value,
+          userName: document.getElementById('uname').value,
+          Password: document.getElementById('pass').value,
+          confirmPassword: document.getElementById('cpwd').value
+        }
+        console.log("Account Information",step);
+    }
+    submit.addEventListener('click',step1);
+
+    var submit1= document.querySelector('#submit2');
+
+    function step2(){
+        var otherStep={
+            FirstName: document.getElementById('fname').value,
+            LastName: document.getElementById('lname').value,
+            contactNumber: document.getElementById('pno').value,
+            contact2: document.getElementById('pno2').value
+        }
+        console.log("Personal Information:",otherStep);
+    }
+    submit1.addEventListener('click',step2);
